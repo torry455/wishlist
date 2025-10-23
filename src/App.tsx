0cbd2023ad4b7e74ab296c1f517d73db363a26e3
torry455 <kucerv8@gmail.com>
2025-10-23 15:42:33 +0300

    Fix routing and deploy config for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import WishPage from './pages/WishPage';
 import { WishProvider } from './context/WishContext';
@@ -8,14 +8,14 @@ const App: React.FC = () => {
   return (
     <div className="dark bg-[#0f0f1a] text-white min-h-screen font-sans">
       <WishProvider>
-        <Router>
+        <BrowserRouter basename="/wishlist">
           <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/wish/:id" element={<WishPage />} />
             </Routes>
           </main>
-        </Router>
+        </BrowserRouter>
       </WishProvider>
     </div>
   );
